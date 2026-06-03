045fa028d015a28c1348dcad8079602e7136c8fc
Analina Nielsen <analinanielsen@outlook.com>
2026-06-02 20:57:21 -0700

    fix logout

--- routes/index.js
+++ routes/index.js
@@ -16,10 +16,20 @@ router.get('/login', passport.authenticate('github'), (req, res) => {});
 
 // I create a logout route so the user can end their session.
 // Passport clears the session and I redirect them back to the home page.
-router.get('/logout', function (req, res, next) {
+// router.get('/logout', function (req, res, next) {
+//     req.logout(function (err) {
+//         if (err) { return next(err);}
+//         res.redirect('/');
+//     });
+// });
+router.get('/logout', (req, res, next) => {
     req.logout(function (err) {
-        if (err) { return next(err);}
-        res.redirect('/');
+        if (err) { return next(err); }
+
+        req.session.destroy(() => {
+            res.clearCookie('connect.sid');
+            res.redirect('/');
+        });
     });
 });
 
